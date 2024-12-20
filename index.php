d21fe0ea1dc6c6e2e788769617b655da7062a08d
lure <158987385+1lure@users.noreply.github.com>
2024-12-20 18:48:26 -0500

    css fix

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>server dashboardd</title>
+    <title>server dashboard</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
